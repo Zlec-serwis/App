07aa5f273ec490129d19422cb4016fae8082cfff
unknown <zlec.serwis@gmail.com>
2019-09-02 23:15:18 +0200

    vendor

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -6,7 +6,7 @@
     <h1>Zlecenia</h1>
     @if(count($posts) > 0)
         @foreach ($posts as $post)
-            <div class="">
+            <div class="cart">
                 <h3><a href="/posts/{{$post->id}}">{{$post->title}}</a></h3>
             <small>Written on {{$post->created_at}}</small>
             </div>
