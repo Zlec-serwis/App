898e1b4ad8b9ff3bd71998df862674c56f347533
marti19686 <marti19686@gmail.com>
2019-10-24 23:31:41 +0200

    poprawa widoku pojedyńczego posta

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -92,7 +92,7 @@ class PostController extends Controller
     public function show($id)
     {
         $post = Post::findOrFail($id);
-        return view('posts.show')->with('post', $post);
+        return view('posts.show', compact('post'));
     }
 
     /**
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,30 +1,48 @@
 @extends('layouts.app')
 
-
 @section('content')
+
 <div class="card">
     <h1>{{$post->title}}</h1>
     <div>
     {!!$post->body!!}
     </div>
     <hr>
-    <small>Dodane by</small>
+        <small>Kategoria</small>
+        @foreach ($post->categories as $category)
+            <small>
+                <a href="">{{ $category->name }}&nbsp;</a>
+            </small>
+        @endforeach
     <hr>
+        <small>Dodane by</small>    
+        <small>{{ $post->user->name }}</small>
     <hr>
-    <small>Kategorie</small>
-    @foreach ($post->categories as $category)
-        <a href="">{{ $category->name }}&nbsp;</a>
-    @endforeach
+        <small>Lokalizacja</small>    
+        <small>{{ $post->address->city }}, {{ $post->address->province }}</small>
     <hr>
-    <a href="/posts" class=""><button type="button" class="card btn btn-default btn-outline-dark">Wstecz</Button></a>
+    
 
-    @if(!Auth::guest())
-        @if(Auth::user()->id == $post->user_id)
-            <a href="/posts/{{$post->id}}/edit" class="btn btn-default btn-outline-dark">Edit</a>
-            {!!Form::open(['action' => ['PostController@destroy', $post->id], 'method' => 'POST', 'class' => 'float-right'])!!}
-            {{Form::hidden('_method', 'DELETE')}}
-            {{Form::submit('Delete', ['class' => 'btn btn-danger'])}}
-            {!!Form::close()!!}
+
+
+    <div class="btn-group " role="group" aria-label="Basic example">
+        <a href="/posts" class="btn btn-default btn-outline-dark">Wstecz</a>
+        @if(!Auth::guest())
+            @if(Auth::user()->id == $post->user_id)
+
+        <a href="/posts/{{$post->id}}/edit" class="btn btn-default btn-outline-dark">Edytuj</a>
+
+
+    </div>
+
+    
+
+    {!!Form::open(['action' => ['PostController@destroy', $post->id], 'method' => 'POST', 'class' => 'float-right'])!!}
+        {{Form::hidden('_method', 'DELETE')}}
+        {{Form::submit('Delete', ['class' => 'btn btn-default btn-danger btn-sm'])}}
+    {!!Form::close()!!}
+
+    
         @endif
     @endif
 @endsection
