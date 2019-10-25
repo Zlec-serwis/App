6e37617396e0392aa3753139759af32428f42986
marti19686 <marti19686@gmail.com>
2019-10-25 19:51:57 +0200

    zmiana w migracjach offer

--- database/migrations/2019_10_25_174811_create_offers_table.php
+++ database/migrations/2019_10_25_174811_create_offers_table.php
@@ -15,6 +15,20 @@ class CreateOffersTable extends Migration
     {
         Schema::create('offers', function (Blueprint $table) {
             $table->bigIncrements('id');
+
+            $table->bigInteger('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+
+            $table->bigInteger('doer_id')->unsigned();
+            $table->foreign('doer_id')->references('id')->on('doers')->onDelete('cascade');
+
+            $table->bigInteger('post_id')->unsigned();
+            $table->foreign('post_id')->references('id')->on('posts')->onDelete('cascade');
+
+            $table->double('price');
+            $table->bigInteger('day');
+            $table->text('description');
+            $table->boolean('accepted')->default('0');
             $table->timestamps();
         });
     }
