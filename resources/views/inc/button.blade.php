5049a08b1c0624b2477e7b9c36a5f8afe6db65b8
marti19686 <marti19686@gmail.com>
2019-12-22 20:28:58 +0100

    Poprawienie dynamicznego menu

--- resources/views/inc/button.blade.php
+++ resources/views/inc/button.blade.php
@@ -1,32 +1,35 @@
 {{ Form::open(['method' => 'GET', 'url' => $url]) }}
-    <nav class="navbar navbar-expand-md">
-        <div class="container">
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav mr-auto">
-                    <li class="nav-link ">
-                        @foreach(\App\Address::orderBy('city', 'ASC')->get() as $address)
-                            {{ Form::radio('city', $address->id, Request::get('city') == $address->id, ['onclick' => 'submit()', 'class' => 'btn']) }}
-                            <label class="btn btn-outline-dark">
-                                <b>{{ $address->city }}</b>
-                            </label>
-                        @endforeach
-                    </li>
-                </ul>
-            </div>
-        </div>
-        </nav>
-    <nav class="navbar navbar-expand-md">
-        <div class="container">
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav mr-auto">
-                    @foreach(\App\Category::orderBy('name', 'ASC')->get() as $category)
-                        {{ Form::radio('category', $category->id, Request::get('category') == $category->id, ['onclick' => 'submit()']) }}
-                        <label class="btn btn-outline-dark">
-                        <b>{{ $category->name }}</b>
-                        </label>
-                    @endforeach
-                </ul>
-            </div>
-        </div>
-    </nav>
+
+<nav class="navbar navbar-expand-md">
+<div class="row">
+
+    @foreach(\App\Address::orderBy('city', 'ASC')->get() as $address)
+
+        <label for="{{ $address->city }}" class="btn btn-outline-dark mr-2">
+            {{ Form::radio('city', $address->id, Request::get('city') == $address->id, ['id'=>$address->city, 'onclick' => 'submit()', 'class' => 'clean']) }}
+            <b class="active">{{ $address->city }}</b>
+        </label>
+
+    @endforeach
+
+</div>
+</nav>
+
+<nav class="navbar navbar-expand-md">
+    <div class="row">
+
+        @foreach(\App\Category::orderBy('name', 'ASC')->get() as $category)
+
+                <label for="{{ $category->name }}" class="btn btn-outline-dark mr-2 ">
+                    {{ Form::radio('category', $category->id, Request::get('category') == $category->id, ['id'=>$category->name, 'onclick' => 'submit()', 'class' => 'clean']) }}
+                    <b class="active">{{ $category->name }}</b>
+                </label>
+
+        @endforeach
+
+    </div>
+</nav>
+
 {{ Form::close() }}
+
+
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -32,5 +32,6 @@
 </div>
 
 <script src="{{ asset('js/app.js') }}" defer></script>
+<script src="{{ asset('js/main.js') }}" defer></script>
 </body>
 </html>
