a05667dde9213c73faa776bff2cb76cea3e85904
marti19686 <marti19686@gmail.com>
2020-01-01 19:45:55 +0100

    update image fix

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -32,7 +32,7 @@ class UserController extends Controller
         if($request->hasFile('avatar')){
             $avatar = $request->file('avatar');
             $filename = time() . '.' . $avatar->getClientOriginalExtension();
-            Image::make($avatar)->resize(300, 300)->save( public_path('/uploads/avatars/' . $filename ));
+            Image::make($avatar)->resize(300, 300)->save( public_path("/uploads/avatars/" . $filename ));
 
             $user = Auth::user();
             $user->avatar = $filename;
