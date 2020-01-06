83a26a0c831087532bd4b8205eae1d77035b14a3
marti19686 <marti19686@gmail.com>
2020-01-06 22:25:44 +0100

    zmiany w uploudzie plików

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -31,8 +31,14 @@ class UserController extends Controller
     public function update_avatar(Request $request) {
         if($request->hasFile('avatar')){
             $avatar = $request->file('avatar');
-            $filename = time() . '.' . $avatar->getClientOriginalExtension();
-            Image::make($avatar)->resize(300, 300)->save( public_path("/uploads/avatars/" . $filename ));
+
+            $filename  = time() . '.' . $avatar->getClientOriginalExtension();
+            $path = public_path('uploads/avatars/' . $filename);
+            Image::make($avatar->getRealPath())->resize(300, 300)->save($path);
+
+
+//            $filename = time() . '.' . $avatar->getClientOriginalExtension();
+//            Image::make($avatar)->resize(300, 300)->save( public_path("/uploads/avatars/" . $filename ));
 
             $user = Auth::user();
             $user->avatar = $filename;
